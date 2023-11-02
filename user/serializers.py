d8d9d194207fea75eedb55b02b39a96d673fc04f
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-02 19:27:46 +0200

    feat: Create UserSerializer for User model

---
+++ user/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "email", "password", "is_staff"]
+        read_only_fields = ["is_staff"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data: dict):
+        """Create new user with encrypted password"""
+        get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(**validated_data)
+
+        if password:
+            user.set_password(password)
+
+        user.save()
+        return user
