eba3c705c2ef8e62ad41426517a3124302513091
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-02 21:41:16 +0200

    feat: Create CreateUserView to register user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from knox.auth import TokenAuthentication
+
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create and save user in Database User table"""
+    serializer_class = UserSerializer
