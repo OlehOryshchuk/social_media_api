18880528e65b76b6b7cd67cd54e0a3d75e934438
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-02 21:47:19 +0200

    feat: Create ManagerUserView which retriever and update current user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -9,3 +11,14 @@ from .serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     """Create and save user in Database User table"""
     serializer_class = UserSerializer
+
+
+class ManagerUserView(generics.RetrieveUpdateAPIView):
+    """Autentication is required. Return current user and
+    user can update his credentials"""
+    serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self) -> get_user_model:
+        return self.request.user
