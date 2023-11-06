fabf1951752783d7367a948d957a1b90687fb8c8
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-06 17:55:04 +0200

    refact: Add username field to UserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class UserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ["id", "email", "password", "is_staff"]
+        fields = ["id", "email", "username", "password", "is_staff"]
         read_only_fields = ["is_staff"]
         extra_kwargs = {"password": {"write_only": True}}
 
