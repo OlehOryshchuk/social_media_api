d52cbfcb9707273fc856484535e1a65479b941a8
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-06 18:41:51 +0200

    refact: Remove signal handler & make username field required in User model

--- user/models.py
+++ user/models.py
@@ -1,12 +1,8 @@
 from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 from django.contrib.auth.models import (
     AbstractUser,
 )
 from django.utils.translation import gettext as _
-
-from social_media.models import Profile
 
 
 class User(AbstractUser):
@@ -15,7 +11,6 @@ class User(AbstractUser):
         _("Username"),
         max_length=30,
         unique=True,
-        blank=True,
     )
 
     # Specifies email as the unique identifier for a user.
@@ -28,14 +23,3 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return f"{self.username} - {self.email}"
-
-
-@receiver(post_save, sender=User)
-def create_profile(
-        sender: post_save,
-        instance: User,
-        created: bool,
-        **kwargs
-):
-    if created and not instance.is_staff:
-        Profile.objects.create(user=instance)
