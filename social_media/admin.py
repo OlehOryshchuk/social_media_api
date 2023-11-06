9679ec16665ad92ceb1e83fb4f765aac148ab42d
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-06 21:47:44 +0200

    feat: Register Profile models in admin site

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
+from .models import Profile, Post
 
-# Register your models here.
+admin.site.unregister(Group)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ["user__username", "user__email"]
+    list_display = [
+        "user",
+        "profile_picture",
+        "bio"
+    ]
