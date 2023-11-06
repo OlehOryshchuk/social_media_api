fdc8aee7005a1a3644b8013b33f5f591d5066916
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-06 17:52:47 +0200

    feat: Delete custom manager and add username field to User model

--- user/models.py
+++ user/models.py
@@ -1,54 +1,41 @@
-from typing import Any
-
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.contrib.auth.models import (
     AbstractUser,
-    BaseUserManager,
 )
 from django.utils.translation import gettext as _
 
-
-class UserManager(BaseUserManager):
-    """Define a model manager for a User model with no username field"""
-
-    use_in_migrations = True
-
-    def _create_user(self, email: str, password: Any, **extra_kwargs):
-        """Create and save a User with password and email field."""
-        if not email:
-            raise ValueError("The email must be set")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_kwargs)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_user(self, email: str, password: Any, **extra_kwargs):
-        """Create and save a regular User with password and email"""
-        extra_kwargs.setdefault("is_staff", False)
-        extra_kwargs.setdefault("is_superuser", False)
-
-        return self._create_user(email, password, **extra_kwargs)
-
-    def create_superuser(self, email: str, password: Any, **extra_kwargs):
-        """Create and save a Superuser with password and email"""
-        extra_kwargs.update({
-            "is_staff": True,
-            "is_superuser": True,
-        })
-
-        return self._create_user(email, password, **extra_kwargs)
+from social_media.models import Profile
 
 
 class User(AbstractUser):
-    username = None
     email = models.EmailField(_("Email address"), unique=True)
+    username = models.CharField(
+        _("Username"),
+        max_length=30,
+        unique=True,
+        blank=True,
+    )
 
     # Specifies email as the unique identifier for a user.
     # and create superuser through email and password
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username"]
 
-    objects = UserManager()
+    class Meta:
+        unique_together = ["email", "username"]
+
+    def __str__(self) -> str:
+        return f"{self.username} - {self.email}"
+
+
+@receiver(post_save, sender=User)
+def create_profile(
+        sender: post_save,
+        instance: User,
+        created: bool,
+        **kwargs
+):
+    if created and not instance.is_staff:
+        Profile.objects.create(user=instance)
