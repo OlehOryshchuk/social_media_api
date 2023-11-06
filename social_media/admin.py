a8d964cc92e789ef623bc8b430a3f4fa70992300
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-06 22:09:42 +0200

    feat: Register Post model in admin site & do some changes in ProfileAdmin

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,15 +1,34 @@
 from django.contrib.auth.models import Group
 from django.contrib import admin
-from .models import Profile, Post
+from .models import (
+    Profile,
+    Post,
+    PostRate,
+    Comment,
+    CommentRate,
+)
 
 admin.site.unregister(Group)
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ["user__username", "user__email"]
+    search_fields = ["user__username"]
     list_display = [
         "user",
         "profile_picture",
         "bio"
     ]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ["author__username"]
+    list_display = [
+        "author",
+        "image",
+        "content",
+        "created_at",
+    ]
+    list_filter = ["tags__name"]
+
