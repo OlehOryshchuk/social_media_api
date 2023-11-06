22daf217781270d867cfd842b7333203afbe8315
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-06 18:42:30 +0200

    feat: Create Profile model with picture file path functions

--- social_media/models.py
+++ social_media/models.py
@@ -7,9 +7,20 @@ from django.conf import settings
 from django.utils.translation import gettext as _
 from django.utils.text import slugify
 
+from user.models import User
+
+
+def get_file_new_name(instance) -> str:
+    """Return filename base on instance"""
+    if isinstance(instance, User):
+        return f"{instance.username}_picture"
+    return f"{instance.user.username}_post"
+
 
 def custom_image_file_path(instance, filename):
     _, ext = os.path.splitext(filename)
+    filename = get_file_new_name(instance)
+    return f"{slugify(filename)}_{uuid.uuid4()}{ext}"
 
 
 class Profile(models.Model):
@@ -29,3 +40,15 @@ class Profile(models.Model):
         blank=True,
         upload_to=custom_image_file_path
     )
+    bio = models.TextField(max_length=1000, blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+    def get_followers(self):
+        """Return profiles that are following current profile"""
+        return self.followers.all()
+
+    def get_followings(self):
+        """Return profiles that current profile is following """
+        return self.followings.all()
