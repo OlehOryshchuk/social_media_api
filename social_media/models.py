2d84a81aad3d4c67afde013b54cfa46996edea4c
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-07 19:33:37 +0200

    style: Remove unnecessary code

--- social_media/models.py
+++ social_media/models.py
@@ -2,14 +2,10 @@ import os
 import uuid
 
 from django.db import models
-from django.core.exceptions import ValidationError
 from django.conf import settings
-from django.utils.translation import gettext as _
 from django.utils.text import slugify
 
 from taggit.managers import TaggableManager
-
-from user.models import User
 
 
 def get_file_new_name(instance) -> str:
