aba06e1ab8e35a35c44ee1af0b74e545c26cc00f
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-16 22:11:23 +0200

    refact/fix: Fix bugs in social_media.serializers

--- social_media/serializers.py
+++ social_media/serializers.py
@@ -1,15 +1,13 @@
 from rest_framework.reverse import reverse
 
 from rest_framework import serializers
+from taggit.serializers import (
+    TaggitSerializer,
+    TagListSerializerField
+)
 from taggit.models import Tag
 
-from .models import (
-    Profile,
-    Post,
-    PostRate,
-    Comment,
-    CommentRate
-)
+from .models import Profile, Post, PostRate, Comment, CommentRate
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -23,9 +21,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 class ProfileListSerializer(ProfileSerializer):
-    username = serializers.CharField(
-        read_only=True, source="user.username"
-    )
+    username = serializers.CharField(read_only=True, source="user.username")
     profile_url = serializers.HyperlinkedIdentityField(
         read_only=True,
         view_name="social_media:profile-detail",
@@ -48,116 +44,115 @@ class ProfileImageUpload(ProfileSerializer):
         fields = ["id", "profile_picture"]
 
 
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = [
-            "id",
-            "content",
-            "image",
-            "tags",
-            "created_at"
-        ]
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = [
-            "id",
-            "content",
-            "reply_to_comment",
-            "created_at"
-        ]
-
-
-class CommentListSerializer(CommentSerializer):
-    """See posts comments, and every comment have number
-    of likes/dislikes/replies and see who is the author of
-    comment"""
-
-    num_of_likes = serializers.IntegerField(read_only=True)
-    num_of_dislikes = serializers.IntegerField(read_only=True)
-    num_of_replies = serializers.IntegerField(read_only=True)
-    replies_url = serializers.HyperlinkedIdentityField(
-        read_only=True,
-        view_name="comments-detail",
-        lookup_field="id"
-    )
-    author = ProfileListSerializer(many=True, read_only=True)
-
-    class Meta(CommentSerializer.Meta):
-        fields = CommentSerializer.Meta.fields + [
-            "id",
-            "author",
-            "num_of_likes",
-            "num_of_dislikes",
-            "num_of_replies"
-        ]
-
-
-class CommentDetailSerializer(CommentSerializer):
-    replies = CommentListSerializer(
-        read_only=True, many=True
-    )
-
-    class Meta:
-        model = Comment
-        fields = ["replies"]
-
-
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ["id", "name"]
 
 
-class TagListSerializer(TagSerializer):
-    detail_url = serializers.HyperlinkedIdentityField(
+class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
+    tags = TagListSerializerField()
+
+    class Meta:
+        model = Post
+        fields = ["id", "content", "image", "tags", "created_at"]
+        extra_kwargs = {"image": {"required": False}}
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "content", "reply_to_comment", "created_at"]
+
+
+class CommentListSerializer(CommentSerializer):
+    """See posts comments, and every comment have number
+    of likes/dislikes/replies and see who is the author of
+    comment"""
+    num_of_likes = serializers.IntegerField(read_only=True)
+    num_of_dislikes = serializers.IntegerField(read_only=True)
+    num_of_replies = serializers.IntegerField(read_only=True)
+    like_url = serializers.HyperlinkedIdentityField(
         read_only=True,
-        view_name="tag-detail",
+        view_name="social_media:comment-like",
+        lookup_url_kwarg="pk"
+    )
+    dislike_url = serializers.HyperlinkedIdentityField(
+        read_only=True,
+        view_name="social_media:comment-dislike",
+        lookup_url_kwarg="pk"
+    )
+    replies_url = serializers.SerializerMethodField()
+    author = ProfileListSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "content",
+            "created_at",
+            "author",
+            "like_url",
+            "dislike_url",
+            "replies_url",
+            "num_of_likes",
+            "num_of_dislikes",
+            "num_of_replies",
+        ]
+
+    def get_replies_url(self, obj: Comment):
+        """Return comment replies endpoint URI(Uniform Resource Identifier)"""
+        return self.context["request"].build_absolute_uri(
+            reverse("social_media:comment-replies", args=[obj.post.id, obj.pk])
+        )
+
+
+class CommentDetailSerializer(CommentSerializer):
+    replies = CommentListSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Comment
+        fields = ["replies"]
+
+
+class TagListSerializer(serializers.ModelSerializer):
+    tag_url = serializers.HyperlinkedIdentityField(
+        read_only=True,
+        view_name="social_media:tag-detail",
     )
 
     class Meta:
         model = Tag
-        fields = ["id", "name", "tag_url"]
+        fields = ["name", "tag_url"]
 
 
 class PostListSerializer(PostSerializer):
     """List of posts, where we can see number of
     likes/dislikes/comments and post tags"""
-    profile_url = serializers.HyperlinkedRelatedField(
-        view_name="social_media:profile-detail",
-        lookup_field="author__id",
-        read_only=True,
-    )
+
+    profile_url = serializers.SerializerMethodField()
     comments_url = serializers.HyperlinkedIdentityField(
         read_only=True,
         view_name="social_media:post-comments",
-        lookup_field="id"
+        lookup_url_kwarg="post_pk",
     )
     like_url = serializers.HyperlinkedIdentityField(
         read_only=True,
         view_name="social_media:post-like",
-        lookup_field="id"
     )
     dislike_url = serializers.HyperlinkedIdentityField(
         read_only=True,
-        view_name="social_media:post-like",
-        lookup_field="id"
+        view_name="social_media:post-dislike",
     )
     num_of_likes = serializers.IntegerField(read_only=True)
     num_of_dislikes = serializers.IntegerField(read_only=True)
     num_of_comments = serializers.IntegerField(read_only=True)
-    tags_url = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="social_media:tag_detail"
-    )
+    tags = TagListSerializer(many=True, read_only=True)
 
     class Meta(PostSerializer.Meta):
         fields = PostSerializer.Meta.fields + [
             "profile_url",
-            "tags_url",
+            "tags",
             "comments_url",
             "like_url",
             "dislike_url",
@@ -166,24 +161,23 @@ class PostListSerializer(PostSerializer):
             "num_of_comments",
         ]
 
+    def get_profile_url(self, obj: Post):
+        """Return profile detail URI(Uniform Resource Identifier)"""
+        return self.context["request"].build_absolute_uri(
+            reverse("social_media:profile-detail", args=[obj.author.id])
+        )
+
 
 class ProfileDetailSerializer(ProfileSerializer):
     """Detail information of current user profile"""
-    num_of_followers = serializers.IntegerField(
-        read_only=True
-    )
-    num_of_followings = serializers.IntegerField(
-        read_only=True
-    )
-    num_of_posts = serializers.IntegerField(
-        read_only=True
-    )
+
+    num_of_followers = serializers.IntegerField(read_only=True)
+    num_of_followings = serializers.IntegerField(read_only=True)
+    num_of_posts = serializers.IntegerField(read_only=True)
     is_following = serializers.SerializerMethodField()
-    posts = serializers.HyperlinkedRelatedField(
-        view_name="profile-posts",
+    posts = serializers.HyperlinkedIdentityField(
+        view_name="social_media:post-profile",
         read_only=True,
-        many=True,
-        lookup_field="id"
     )
 
     def get_is_following(self, obj):
