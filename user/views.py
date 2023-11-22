e7da0d6d6801b2155654d144fcf264af9a6b32cc
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-11-22 22:01:07 +0200

    feat: Override djoser.views.PasswordResetEmail to use our custom template

--- user/views.py
+++ user/views.py
@@ -1,10 +1,17 @@
+from django.urls.base import reverse_lazy
+
 from rest_framework.response import Response
 from rest_framework import status
 
 from djoser.views import UserViewSet as DjoserUserViewSet
+from djoser.email import PasswordResetEmail as DjoserPasswordResetEmail
 
 
 class UserViewSet(DjoserUserViewSet):
     """Unable list endpoint from DjoserUserViewSet"""
     def list(self, request, *args, **kwargs):
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class PasswordResetEmail(DjoserPasswordResetEmail):
+    template_name = "registration/password_reset_email.html"
