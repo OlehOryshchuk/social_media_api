021f22e42290bbf0a4ecb9f31770ad4222b7c256
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-06 20:47:52 +0200

    refact: Remove static configurations

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -147,13 +147,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 #
 STATIC_URL = "static/"
-STATIC_ROOT = "/static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 # media
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -41,5 +41,3 @@ urlpatterns = [
     ),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
